da4908d139f7e1615e1e56b7343f91ac500bd5dd
Kartikaya Gupta <kartikaya@squareup.com>
2023-04-20 22:01:59 -0400

    Split plugin into separate folder

--- 20230420-decorated-task/build.gradle.kts
+++ 20230420-decorated-task/build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+        classpath(fileTree("../plugin/build/libs/") { include("plugin.jar") })
     }
 }
 
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Run:
 
 ```
-bin/gradle project-b:shadowJar
+cd plugin
+../bin/gradle jar
+cd ../20230420-decorated-task
+../bin/gradle project-b:shadowJar
 ```
